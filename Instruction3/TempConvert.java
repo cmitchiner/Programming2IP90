205bb82f273c5d1a383dced290a8cf9729ba4caf
cmitchiner <c.l.mitchiner@student.tue.nl>
2020-09-08 15:08:35 +0200

    test

--- Instruction3/TempConvert.java
+++ Instruction3/TempConvert.java
@@ -33,12 +33,7 @@ public class TempConvert
                 System.out.println("Error");
             }
         }
-<<<<<<< HEAD
         //CharlieComment
-=======
-        // comment
->>>>>>> de9bd30d7439b0352263b22e32597e808d547e8c
-
     }
     public static void main(String[] args)
     {
