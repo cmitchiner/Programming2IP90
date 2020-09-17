a5ae577930296465a5236cffa5e369aaaa99d8a8
cmitchiner <c.l.mitchiner@student.tue.nl>
2020-09-17 18:52:07 +0200

    we done

--- HW3/Cellulitis.java
+++ HW3/Cellulitis.java
@@ -13,8 +13,9 @@ class Cellulitis {
   Scanner scanner = new Scanner(System.in);
   int rowLength;
   int numOfGens;
-  
-  boolean[] rulesetB = {false, true, false, true, false, true, true, false};
+
+  int[] ruleSetUInt = new int[8];
+  Boolean[] ruleSetU = new Boolean[8];
 
   void run(){
 
@@ -37,7 +38,7 @@ class Cellulitis {
       }
     }
     String initEnd = scanner.next(); //Why is this here, when we can use .hasNextInt()
-    
+
     if (whichAuto == 'a')//If its auto A, follow rules for A
     {
       currentGeneration = nextGenerationA(currentGeneration);
@@ -45,6 +46,20 @@ class Cellulitis {
     if (whichAuto == 'b')//If its auto B, follow rules for B
     {
       currentGeneration = nextGenerationB(currentGeneration);
+    }
+    if (whichAuto == 'u'){
+      for(int count = 0; count < 8; count++){
+        ruleSetUInt[count] = scanner.nextInt();
+      }
+      for(int count = 0; count < 8; count++){
+      if (ruleSetUInt[count] == 1) {
+        ruleSetU[count] = true;
+      }
+      else {
+        ruleSetU[count] = false;
+      }
+    }
+      currentGeneration = nextGenerationU(currentGeneration);
     }
     //draw(currentGeneration);//Print out the generations
     
@@ -123,6 +138,36 @@ class Cellulitis {
     return false;
   }
 
+  boolean rulesAutomataU(boolean a, boolean b, boolean c)
+  {
+
+    if (a == false && b == false && c == false){
+      return ruleSetU[0];
+    }
+    else if (a == false && b == false && c == true){
+      return ruleSetU[1];
+    }
+    else if (a == false && b == true && c == false){
+      return ruleSetU[2];
+    }
+    else if (a == false && b == true && c == true){
+      return ruleSetU[3];
+    }
+    else if (a == true && b == false && c == false){
+      return ruleSetU[4];
+    }
+    else if (a == true && b == false && c == true){
+      return ruleSetU[5];
+    }
+    else if (a == true && b == true && c == false){
+      return ruleSetU[6];
+    }
+    else if (a == true && b == true && c == true){
+      return ruleSetU[7];
+    }
+    return false;
+  }
+
   boolean[] generate(boolean[] generation, char r)
   {
     boolean[] newArray = new boolean[rowLength + 2];
@@ -160,6 +205,23 @@ class Cellulitis {
       }
       return newArray;
     }
+    else if (r == 'u'){
+      for (int j = 1; j <= rowLength; j++)
+      {
+          if (generation[j] == true)
+          {
+            try {
+              newArray[j-1] = rulesAutomataU(generation[j-2], generation[j-1], generation[j]);
+              newArray[j] =  rulesAutomataU(generation[j-1], generation[j], generation[j+1]);
+              newArray[j+1] = rulesAutomataU(generation[j], generation[j+1], generation[j+2]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+              j++;
+            }
+              
+          }
+      }
+      return newArray;
+    }
     return null;
   }
   boolean[] nextGenerationA(boolean[] generation){
@@ -188,7 +250,19 @@ class Cellulitis {
     //Change the generation array for the next row following rules for A
     return generation;
   }
-  
+  boolean[] nextGenerationU(boolean[] generation){
+    //Calculates the next generation from the provided generation according to
+    //Rules for automaton A
+    draw(generation); //First line
+    for (int i = 1; i < numOfGens; i++)
+    {
+        generation = generate(generation, 'u');
+        System.out.println();
+        draw(generation);
+    }
+    //Change the generation array for the next row following rules for A
+    return generation;
+  }
   public static void main(String[] args) { 
     new Cellulitis().run();
   }
