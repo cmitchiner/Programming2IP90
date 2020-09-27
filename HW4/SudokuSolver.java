e7ec8537ce7bfe3309ca0448d97832edfe06c300
cmitchiner <c.l.mitchiner@student.tue.nl>
2020-09-27 19:39:19 +0200

    asda

--- HW4/SudokuSolver.java
+++ HW4/SudokuSolver.java
@@ -7,7 +7,6 @@
  * Date: 24th September 2020
  */
 class SudokuSolver {
-    static final int SUDUKU_GRID_MIN_INDEX = 0; //Minimum value for a row's index or a col's index
     static final int SUDOKU_SIZE = 9;          // Size of the grid.
     static final int SUDOKU_MIN_NUMBER = 1;    // Minimum digit to be filled in.
     static final int SUDOKU_MAX_NUMBER = 9;    // Maximum digit to be filled in.
@@ -15,46 +14,31 @@ class SudokuSolver {
     static final int  EMPTY = 0;                // If a cell is empty it equals 0
     
     int[][] grid = new int[][] {  // The puzzle grid; 0 represents empty.
-        { 0, 9, 0,   7, 3, 0,    4, 0, 0 },    // One solution.
-        { 0, 0, 0,   0, 0, 0,    5, 0, 0 },
-        { 3, 0, 0,   0, 0, 6,    0, 0, 0 },
+        { 7, 0, 0,   4, 0, 0,    2, 0, 0 },    // One solution.
+        { 9, 3, 0,   0, 0, 0,    0, 0, 0 },
+        { 0, 0, 2,   0, 8, 0,    0, 0, 0 },
 
-        { 0, 0, 0,   0, 0, 2,    6, 4, 0 },
-        { 0, 0, 0,   6, 5, 1,    0, 0, 0 },
-        { 0, 0, 6,   9, 0, 7,    0, 0, 0 },
+        { 4, 0, 0,   0, 0, 2,    0, 9, 0 },
+        { 0, 0, 0,   0, 0, 0,    0, 0, 6 },
+        { 0, 0, 1,   8, 0, 6,    0, 7, 0 },
 
-        { 5, 8, 0,   0, 0, 0,    0, 0, 0 },
-        { 9, 0, 0,   0, 0, 3,    0, 2, 5 },
-        { 6, 0, 3,   0, 0, 0,    8, 0, 0 },
+        { 0, 0, 0,   0, 0, 3,    0, 0, 0 },
+        { 0, 0, 0,   9, 0, 5,    0, 0, 0 },
+        { 0, 0, 0,   0, 0, 0,    0, 4, 0 },
     };
 
     int solutionCounter = 0; // Solution counter
 
-    private int[][] board;
-
-    //Fills our secondary array "board" with passed argument
-    void fillSecondaryArray() {  
-        this.board = new int[SUDOKU_SIZE][SUDOKU_SIZE];
-
-        for (int i = SUDUKU_GRID_MIN_INDEX; i < SUDOKU_SIZE; i++) {
-            for (int j = SUDUKU_GRID_MIN_INDEX; j < SUDOKU_SIZE; j++) {
-                this.board[i][j] = grid[i][j];
-            }
-        }
-    }
-
     // Is there a conflict when we fill in d at position (r, c)?
     boolean givesConflict(int r, int c, int d) {
-        if (!rowConflict(r, d) && !columnConflict(c, d) && !boxConflict(r, c, d) && !asteriskConflict(board)) { //If there isnt a conflict in any of the conditions, return false
-            return false;
-        }
-        return true; //if any of the 4 conditions have conflict, return true
+        return !(!rowConflict(r, d) && !columnConflict(c, d) && !boxConflict(r, c, d) && !asteriskConflict(grid)); //If there isnt a conflict in any of the conditions, return false
+        //False if no conflict, true if there is conflict
     }
 
     // Is there a conflict when we fill in d in row r?
     boolean rowConflict(int r, int d) {
-        for (int i = SUDUKU_GRID_MIN_INDEX; i < SUDOKU_SIZE; i++) { //Check every column for the passed paramter index for row
-            if (board[r][i] == d) {     
+        for (int i = SUDOKU_MIN_NUMBER - 1; i < SUDOKU_SIZE; i++) { //Check every column for the passed paramter index for row
+            if (grid[r][i] == d) {     
                 return true;
             }
         }
@@ -63,8 +47,8 @@ class SudokuSolver {
 
     // Is there a conflict in column c when we fill in d?
     boolean columnConflict(int c, int d) {
-        for (int i = SUDUKU_GRID_MIN_INDEX; i < SUDOKU_SIZE; i++) {  //Check every row for the passed paramter index for col
-            if (board[i][c] == d){
+        for (int i = SUDOKU_MIN_NUMBER - 1; i < SUDOKU_SIZE; i++) {  //Check every row for the passed paramter index for col
+            if (grid[i][c] == d){
                 return true;
             }
         }
@@ -78,7 +62,7 @@ class SudokuSolver {
 
         for (int i = row; i < row + SUDOKU_BOX_DIMENSION; i++) {
             for (int j = col; j < col + SUDOKU_BOX_DIMENSION; j++) {
-                if (board[i][j] == d){
+                if (grid[i][j] == d){
                     return true;
                 }
             }
@@ -114,41 +98,88 @@ class SudokuSolver {
         }
         return true;
     }
-
-    // Find all solutions for the grid, and stores the final solution.
-    boolean solve() {
-        for (int row = SUDUKU_GRID_MIN_INDEX; row < SUDOKU_SIZE; row++) {               //Loops through every row
-            for (int column = SUDUKU_GRID_MIN_INDEX; column < SUDOKU_SIZE; column++) { //Loops through every col
-                if (board[row][column] == EMPTY) {                                     //If a spot we can move something into it
-                    for (int num = SUDOKU_MIN_NUMBER; num <= SUDOKU_MAX_NUMBER; num++) { //Loop through every possible option to put into the empty spot
-                        if (!givesConflict(row, column, num)) {                          //If there is no conflict with adding a number "num" at a spot add it to that spot
-                            board[row][column] = num;
-                            if (solve()){                                                 //Now recursivly backtrack and repeat                                           
-                                return true;
-                            } else {
-                                board[row][column] = EMPTY;                              // If we returned false i.e there was conflict on all numbers 1-9, reset that space to EMPTY                        
-                            }
-                        }
-                    }
-                    solutionCounter++;
-                    return false;                                                         //If all numbers 1-9 give conflict recurisvly backtrack until they dont
-                }
+    boolean isAnAsteriskIndex(int row, int col)
+    {
+        int[] asteriskRowIndex = {2, 1, 2, 4, 4, 4, 6, 7, 6};
+        int[] asteriskColIndex = {2, 4, 6, 1, 4, 7, 2, 4, 6};
+        for (int i = 0; i < 9; i++)
+        {
+            if (asteriskRowIndex[i] == row && asteriskColIndex[i] == col){
+                return true;
             }
         }
-        return true; //Puzzle has been solved
+        return false;
     }
 
+    // Find all solutions for the grid, and stores the final solution.
+    public int[][] board2 = new int[SUDOKU_SIZE][SUDOKU_SIZE];
+    public int solve(int[][] board, int solutionCounter)
+    {
+        for (int row = 0; row < SUDOKU_SIZE; row++) { //Increment through rows
+
+            for (int column = SUDOKU_MIN_NUMBER - 1; column < SUDOKU_SIZE; column++) { //Increment through columns
+
+              if (board[row][column] == EMPTY) { 
+
+                for (int num = SUDOKU_MIN_NUMBER; num <= SUDOKU_MAX_NUMBER; num++) { //Increment through possible number choices
+
+                  if (!givesConflict(row, column, num)) { 
+
+                    board[row][column] = num;                   //If no conflict, set that spot to num
+
+                    int cache = solve(board, solutionCounter);  //Keep track of previous solutions
+
+                    //Once we have found one solution, we must backtrack again but force one of our solved values to increment, then check for a solution
+                    if (cache > solutionCounter) {
+                        solutionCounter = cache;
+                      for (int i = SUDOKU_MIN_NUMBER - 1; i < SUDOKU_SIZE; i++) {
+                        for (int j = SUDOKU_MIN_NUMBER - 1; j < SUDOKU_SIZE; j++) {
+                          if (board[i][j] != EMPTY) {
+                            board2[i][j] = board[i][j];
+                          }
+                        }
+                      }
+                      board[row][column] = EMPTY;
+                    } else {
+                      board[row][column] = EMPTY;
+                    }
+                  }
+                //   else if (!rowConflict(row, num) && !columnConflict(column, num) && !boxConflict(row, column, num) && !isAnAsteriskIndex(row, column)){
+                //     board[row][column] = num;
+                //     int cache = solve(board, solutionCounter);
+                //     if (cache > solutionCounter) {
+                //         //solutionCounter = cache;
+                //       for (int i = SUDOKU_MIN_NUMBER - 1; i < SUDOKU_SIZE; i++) {
+                //         for (int j = SUDOKU_MIN_NUMBER - 1; j < SUDOKU_SIZE; j++) {
+                //           if (board[i][j] != EMPTY) {
+                //             board2[i][j] = board[i][j];
+                //           }
+                //         }
+                //       }
+                //       board[row][column] = EMPTY;
+                //     } else {
+                //       board[row][column] = EMPTY;
+                //     }
+                //   }
+                }
+                return solutionCounter;
+              }
+            }
+          }
+          return solutionCounter + 1;
+      }
+
     // Print the sudoku grid.
-    void print() {
+    void print(int[][] board) {
         System.out.println("+-----------------+"); //Top bar
-        for (int i = SUDUKU_GRID_MIN_INDEX; i < SUDOKU_SIZE; i++) {
+        for (int i = SUDOKU_MIN_NUMBER - 1; i < SUDOKU_SIZE; i++) {
             
             if (i == 3 || i == 6){
                 System.out.print("+-----------------+\n|"); //Two middle bars
             } else {
                 System.out.print("|"); //Prionts first row of |
             }
-            for (int j = SUDUKU_GRID_MIN_INDEX; j < SUDOKU_SIZE; j++) {
+            for (int j = SUDOKU_MIN_NUMBER - 1; j < SUDOKU_SIZE; j++) {
                 if (j != 8) {
                     if ((i == 1 && j == 3) || (i == 4 && j == 3) || (i == 7 && j == 3) || (i == 4 && j == 6)){ //Hardcode asterisks locations
                         System.out.print("|" + board[i][j] + ">");
@@ -181,14 +212,16 @@ class SudokuSolver {
 
     // Run the actual solver.
     void solveIt() {
-        fillSecondaryArray();
-        if (solve()){                         //If solve returned true, the puzzle is solved
-            print();                          //Thus print out the grid
-
-        } else {
-            System.out.println("Unsolvable"); //If the last resursive call returns false, the puzzle was unsolvable
+        solutionCounter = solve(grid, solutionCounter);
+        if (solutionCounter == 1) { //If one solution, print it
+            print(board2);
+        } else if (solutionCounter > 1){
+            System.out.println(solutionCounter); //Otherwise print how many solutions
         }
-        //System.out.println(solutionCounter); //Number of failed attempts to insert numbers
+        else
+        {
+            print(board2);
+        }
     }
 
     public static void main(String[] args) {
